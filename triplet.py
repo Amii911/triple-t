9d463d215691ce5755971ce3a9ce89f3f5919cf8
Aminat Adebayo <adebayo.aminat@gmail.com>
2026-08-09 20:01:46 -0400

    creating board

--- triplet.py
+++ triplet.py
@@ -2,6 +2,10 @@ board = [ "_" , "_", "_",
           "_" , "_", "_",
           "_",  "_", "_"]
 
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
 def printBoard(board):
     print(board[0] + "|" + board[1] + "|" + board[3])
     print(board[4] + "|" + board[5] + "|" + board[6])
